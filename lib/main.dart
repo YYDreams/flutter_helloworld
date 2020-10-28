301ad4448bc96310a08fbb4e156e6e61d9ecbf10
huahua <799431668@qq.com>
2020-10-28 20:19:55 +0800

    add 选择时间空间初始化

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/selected_time_page.dart';
 
 main() => (runApp(MyApp()));
 
@@ -21,7 +22,45 @@ class MyApp extends StatelessWidget {
 class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.only(top: 50.0),
+      child:  Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom: 16.0),
+                child: Text("sdsds"),
+//                  child: Text(selectArea != null ? selectArea['address'] : '未选择...'),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: (){
+                  showBottomSheet(context: context, builder: (_)=>Container(
+                    height: 300.0,
+                    child: SelectedTimePage(
+//                        onSelect: handleSelect,
+//                        initProviceIndex: selectArea == null ? 0 : selectArea['proviceIndex'],
+//                        initCityIndex: selectArea == null ? 0 : selectArea['cityIndex'],
+//                        initCountyIndex: selectArea == null ? 0 : selectArea['countyIndex'],
+                    ),
+                  ));
+                },
+                child: Text('点击选择省市区'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
+
+
+
 
